a7046037d7f29581da0e340adbbe3f775e1c40ec
ritwik-satpati <74023275+ritwik-satpati@users.noreply.github.com>
2026-06-25 16:01:39 +0530

    cleaning up...

--- hardcodes/prompt_manager.py
+++ hardcodes/prompt_manager.py
@@ -8,10 +8,23 @@ def get_prompt(request_id):
     """
     This function returns a hardcoded prompt
     """
-    
-    prompt = "Explain API in simple words within 100 words"
-    
-    # Updating log entry 
-    write_log(filename=request_id, message=f"{MODULE_NAME} | SUCCESS | Base Prompt loaded")
 
-    return prompt
+    try:
+        import os
+        
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(BASE_DIR, "prompt.txt")
+
+        with open(file_path, "r", encoding="utf-8") as f:
+            prompt = f.read().strip()
+
+        # Updating log entry 
+        write_log(filename=request_id, message=f"{MODULE_NAME} | SUCCESS | Base Prompt loaded")
+        
+        return prompt
+ 
+    except Exception as e:
+        # Updating log entry 
+        write_log(filename=request_id, message=f"{MODULE_NAME} | FAILED | Base Prompt not loaded")
+        write_log(filename=request_id, message=f"ERROR : {str(e)}")
+        return None
